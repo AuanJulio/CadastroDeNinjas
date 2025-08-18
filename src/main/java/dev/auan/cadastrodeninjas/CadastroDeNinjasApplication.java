afac636e6741dd1d3b242a35dae9c3e6247e54bf
AuanJulio <auanjulio13@gmail.com>
2025-08-17 22:44:24 -0300

    first commit

---
+++ src/main/java/dev/auan/cadastrodeninjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.auan.cadastrodeninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroDeNinjasApplication.class, args);
+    }
+
+}
