2a01a83caa2649108de1863694310ca40aa12603
AuanJulio <auanjulio13@gmail.com>
2025-09-06 18:38:54 -0300

    feat: coloca o Lombok nas classes para abstração

--- src/main/java/dev/auan/cadastrodeninjas/Missoes/MissaoModel.java
+++ src/main/java/dev/auan/cadastrodeninjas/Missoes/MissaoModel.java
@@ -2,11 +2,17 @@ package dev.auan.cadastrodeninjas.Missoes;
 
 import dev.auan.cadastrodeninjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missao")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class MissaoModel {
 
     @Id
@@ -21,41 +27,4 @@ public class MissaoModel {
     @OneToMany(mappedBy = "missao")
     private List<NinjaModel> ninja;
 
-    public MissaoModel() {
-    }
-
-    public MissaoModel(Long id, String nome, String dificuldade, List<NinjaModel> ninja) {
-        this.id = id;
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-        this.ninja = ninja;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public NinjaModel getNinja() {
-        return ninja;
-    }
-
-    public void setNinja(NinjaModel ninja) {
-        this.ninja = ninja;
-    }
 }
